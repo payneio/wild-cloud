11b6bd0de1b55ea739698701cbef14719e734a03
Paul Payne <paul@payne.io>
2025-05-18 15:15:59 -0700

    Add configuration files for Ghost, MariaDB, Nextcloud, Postgres, and Redis; implement generate-config script and enhance load-env script for dependency management

--- infrastructure_setup/setup-utils.sh
+++ infrastructure_setup/setup-utils.sh
@@ -8,8 +8,30 @@ cd "$SCRIPT_DIR"
 # Install gomplate
 if command -v gomplate &> /dev/null; then
     echo "gomplate is already installed."
-    exit 0
+else
+    curl -sSL https://github.com/hairyhenderson/gomplate/releases/latest/download/gomplate_linux-amd64 -o $HOME/.local/bin/gomplate
+    chmod +x $HOME/.local/bin/gomplate
+    echo "gomplate installed successfully."
 fi
-curl -sSL https://github.com/hairyhenderson/gomplate/releases/latest/download/gomplate_linux-amd64 -o $HOME/.local/bin/gomplate
-chmod +x $HOME/.local/bin/gomplate
-echo "gomplate installed successfully."
+
+# Install kustomize
+if command -v kustomize &> /dev/null; then
+    echo "kustomize is already installed."
+else
+    curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" | bash
+    mv kustomize $HOME/.local/bin/
+    echo "kustomize installed successfully."
+fi
+
+## Install yq
+if command -v yq &> /dev/null; then
+    echo "yq is already installed."
+else
+    VERSION=v4.45.4
+    BINARY=yq_linux_amd64
+    wget https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY}.tar.gz -O - | tar xz
+    mv ${BINARY} $HOME/.local/bin/yq
+    chmod +x $HOME/.local/bin/yq
+    rm yq.1
+    echo "yq installed successfully."
+fi
